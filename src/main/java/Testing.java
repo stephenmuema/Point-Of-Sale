2a7dc68e3c3c2065fd996ab791d04f6e5e25d2d8
Muemasn <muemasn@outlook.com>
2019-10-23 20:11:05 +0300

    CODE FOR GDRIVE FOLDER NOT WORKING BUT CODE FOR GDRIVE HOME IS OPERATIONAL

---
+++ src/main/java/Testing.java
@@ -0,0 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Testing {
+    public static void main(String[] args) throws SQLException {
+// change user and password as you need it
+        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "nanotech");
+
+        ResultSet rs = con.getMetaData().getCatalogs();
+
+        while (rs.next()) {
+            System.out.println("TABLE_CAT = " + rs.getString("TABLE_CAT"));
+        }
+    }
+}
--- src/main/java/testing.java
+++
@@ -1,7 +0,0 @@
-//import org.json.JSONException;
-//import org.json.JSONObject;
-//
-//import java.io.*;
-//import java.net.URL;
-//import java.nio.charset.StandardCharsets;
-//
